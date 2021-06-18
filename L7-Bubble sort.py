7b831e9a7edc6d3ec887630a8cea76854a10439b
megwu1129 <73263355+megwu1129@users.noreply.github.com>
2021-06-18 15:18:07 +0800

    Create L7-Bubble sort.py

---
+++ L7-Bubble sort.py
@@ -0,0 +1,13 @@
+def bubbleSort(arr):
+    n = len(arr)
+    for i in range(n):
+        swapped = False
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                swapped = True
+
+        # If nothing needs to be swapped,
+        # arr is now sorted
+        if swapped is False:
+            break
