27f95a3ff908d0f7b82ece3cc10e4cda44f28868
megwu1129 <73263355+megwu1129@users.noreply.github.com>
2021-06-25 14:47:36 +0800

    Update 75. Sort Colors.py

--- 75. Sort Colors.py
+++ 75. Sort Colors.py
@@ -15,3 +15,17 @@ class Solution:
             else:
                 white+=1
         return nums
+    
+    # count sort    
+def sortColors1(self, nums):
+    c0 = c1 = c2 = 0
+    for num in nums:
+        if num == 0:
+            c0 += 1
+        elif num == 1:
+            c1 += 1
+        else:
+            c2 += 1
+    nums[:c0] = [0] * c0
+    nums[c0:c0+c1] = [1] * c1
+    nums[c0+c1:] = [2] * c2
