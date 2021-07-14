203367d0e8f434cd4ba5519bf170e280eac80cd2
megwu1129 <73263355+megwu1129@users.noreply.github.com>
2021-07-15 00:46:49 +0800

    Update 1004. Max Consecutive Ones III.py

--- 1004. Max Consecutive Ones III.py
+++ 1004. Max Consecutive Ones III.py
@@ -11,3 +11,15 @@ class Solution:
                 i+=1
             longest = max(longest, j-i+1)
         return longest
+    
+    
+        
+class Solution:
+    def longestOnes(self, A: List[int], K: int) -> int:
+        left = 0
+        for right in range(len(A)):
+            K -= 1 - A[right]
+            if K < 0:
+                K += 1 - A[left]
+                left += 1
+        return right - left + 1
