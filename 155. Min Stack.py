7eca8136dd53c97f2f3a891c6a289aeac03f6b59
megwu1129 <73263355+megwu1129@users.noreply.github.com>
2021-07-16 16:06:28 +0800

    Create 155. Min Stack.py

---
+++ 155. Min Stack.py
@@ -0,0 +1,26 @@
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        if len(self.stack) == 0:
+            self.stack.append((val, val))
+        else:
+            if val < self.stack[-1][1]:
+                self.stack.append((val, val))
+            else:
+                self.stack.append((val, self.stack[-1][1]))
+        
+    def pop(self) -> None:
+        self.stack.pop()
+        
+
+    def top(self) -> int:
+        return self.stack[-1][0]
+
+    def getMin(self) -> int:
+        return self.stack[-1][1]    
