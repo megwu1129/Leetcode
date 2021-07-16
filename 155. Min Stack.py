69e99e928a9d2e170cc5be703b28c3f6b0fa7256
megwu1129 <73263355+megwu1129@users.noreply.github.com>
2021-07-16 16:09:53 +0800

    Update 155. Min Stack.py

--- 155. Min Stack.py
+++ 155. Min Stack.py
@@ -7,14 +7,11 @@ class MinStack:
         self.stack = []
 
     def push(self, val: int) -> None:
-        if len(self.stack) == 0:
+        if len(self.stack) == 0 or val < self.stack[-1][1]:
             self.stack.append((val, val))
             return
         else:
-            if val < self.stack[-1][1]:
-                self.stack.append((val, val))
-            else:
-                self.stack.append((val, self.stack[-1][1]))
+            self.stack.append((val, self.stack[-1][1]))
         
     def pop(self) -> None:
         self.stack.pop()
@@ -24,4 +21,4 @@ class MinStack:
         return self.stack[-1][0]
 
     def getMin(self) -> int:
-        return self.stack[-1][1]    
+        return self.stack[-1][1]  
